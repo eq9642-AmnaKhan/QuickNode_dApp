f794882a9d8de514b1e9ed7cfa05f8949c2a32c3
amnakhan82@gmail.com <38444180+1bluebird@users.noreply.github.com>
2022-11-28 00:30:50 -0600

    front end html and css

--- scripts/rpc.js
+++ scripts/rpc.js
@@ -1,6 +1,8 @@
-const ethers = require("ethers");
-(async () => {
-  const provider = new ethers.providers.JsonRpcProvider("https://sleek-blissful-pool.ethereum-goerli.discover.quiknode.pro/3fee6cecb62cfa5d56a22959c8d1f3db12bf71a2/");
-  const blockNum = await provider.getBlockNumber();
-  console.log(blockNum);
-})();
+// const ethers = require("ethers");
+// (async () => {
+//   const provider = new ethers.providers.JsonRpcProvider("https://sleek-blissful-pool.ethereum-goerli.discover.quiknode.pro/3fee6cecb62cfa5d56a22959c8d1f3db12bf71a2/");
+//   const blockNum = await provider.getBlockNumber();
+//   console.log(blockNum);
+// })();
+
+console.log("hello from index.html ...working");
